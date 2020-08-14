0916ce03f0d91c3ad8ccd4b01a7e65a2bd8ea453
Nicat.dcw <59221034+Nicat-dcw@users.noreply.github.com>
2020-08-14 18:37:04 +0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const router = express ();
+
+router.get("/", function(req,res){
+ res.write("AnaSayfaya Hosgeldin");
+}
+// Bir Redirect Formu olusturalim
+router.get("/davet", function(req,res){
+ res.redirect("https://web.kurtbot.tk/bot/add");
+ //Kendinize Göre Ayarlaya Bilirsiniz
+}
