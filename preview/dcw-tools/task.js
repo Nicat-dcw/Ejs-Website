4519e2bd22b2fd742d1ccb73145fd467bee47951
Nicat.dcw <59221034+Nicat-dcw@users.noreply.github.com>
2020-08-15 15:50:08 +0400

    Update task.js

--- preview/dcw-tools/task.js
+++ preview/dcw-tools/task.js
@@ -1,1 +1,16 @@
-// Nicat.dcw //
+// 
+* Author: NicatDcW
+* Document: https://nicatdcw.tk/files/websites/ejs-website/preview/dcw-tools/task.js
+//
+
+const dcw = require('https://nicatdcw.tk/assets/modul/dcw/');
+
+if(!ready==='on'{
+document.write(" File Is Activate ");
+else {
+document.write(" File Is DeActive ");
+}
+});
+function myFunction() {
+document.querySelector(".ready").style.backgroundColor = "red";
+}
